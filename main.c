a384109a019c96fb48b4a4659b5d87236d5943c0
Wagner <vagner.aa@edu.spbstu.ru>
2023-11-28 22:23:47 +0300

    c code

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "ISort.h"
+int main() {
+    int a[10] = {-10, 0, 10, 9, 8, 7, 1, 2, 5, 5}
+    ISort(10, a)
+    for (int i = 0; i<10; i++)
+        printf("%d ", a[i]);
+    return 0;
+}
